f662d34cdcef7d18504b3afbf8bd18ddca4a5cc6
selman alshemali <justin22wi@gmail.com>
2026-01-20 09:58:18 +0100

    add AnimatedList widget

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,7 @@ import 'widget/animated_align_widget.dart';
 import 'widget/animated_container_widget.dart';
 import 'widget/animated_cross_fade_widget.dart' show AnimatedCrossFadeWidget;
 import 'widget/animated_icon_widget.dart';
+import 'widget/animated_list_widget.dart';
 import 'widget/orientation_builder_widget.dart';
 import 'widget/over_flow_box_widget.dart';
 import 'widget/overflow_bar_widget.dart';
@@ -51,7 +52,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AnimatedIconWidget(),
+      home: AnimatedListWidget(),
     );
   }
 }
---
+++ lib/widget/animated_list_widget.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class AnimatedListWidget extends StatefulWidget {
+  const AnimatedListWidget({super.key});
+
+  @override
+  State<AnimatedListWidget> createState() => _AnimatedListWidgetState();
+}
+
+class _AnimatedListWidgetState extends State<AnimatedListWidget> {
+  final _items = [];
+  final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
+  void _addItem() {
+    _items.insert(0, "Items${_items.length}");
+    _key.currentState!.insertItem(0, duration: const Duration(seconds: 1));
+  }
+
+  void _removeItem(int index) {
+    _key.currentState!.removeItem(index, (_, animated) {
+      return SizeTransition(
+        axis: Axis.vertical,
+        sizeFactor: animated,
+        child: const Card(
+          margin: EdgeInsets.all(10),
+          child: ListTile(title: Text("Deleted")),
+        ),
+      );
+    }, duration: const Duration(seconds: 1));
+    _items.removeAt(index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            ElevatedButton(onPressed: _addItem, child: const Text("Add Item")),
+            Expanded(
+              child: AnimatedList(
+                key: _key,
+                initialItemCount: _items.length,
+                itemBuilder: (context, index, animation) {
+                  return SizeTransition(
+                    // axis: Axis.vertical,
+                    key: UniqueKey(),
+                    sizeFactor: animation,
+                    child: Card(
+                      margin: EdgeInsets.all(10),
+                      child: ListTile(
+                        title: Text(_items[index]),
+                        trailing: IconButton(
+                          icon: const Icon(Icons.delete),
+                          onPressed: () => _removeItem(index),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
